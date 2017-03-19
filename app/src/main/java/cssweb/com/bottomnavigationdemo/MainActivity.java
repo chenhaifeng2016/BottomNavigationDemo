f98af8bae787076ee3817844304366ebe271877b
anders chen <chenhf2010@qq.com>
2017-03-19 10:13:40 +0800

    Initial commit

---
+++ app/src/main/java/cssweb/com/bottomnavigationdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package cssweb.com.bottomnavigationdemo;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        break;
+                    case R.id.item2:
+                        break;
+                    case R.id.item3:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
